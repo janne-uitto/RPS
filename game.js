b0fc0b7d5288656cee65c143e2cfa557636f9d19
Janne Uitto <janne.uitto@student.tut.fi>
2018-03-30 22:41:11 +0300

    Functionality finalization

--- game.js
+++ game.js
@@ -1,20 +1,29 @@
-
 /* global $, alert */
 
 $(document).ready( function() {
 "use strict";
 
+$("#over").hide();
+
 // These variable track the state of this "game"
 var streak = 0;
 var points = 0;
+var lives = 10;
 
 // Send score to service.
 function gameOver() {
   var msg = {
 	"messageType": "SCORE",
-	"score": parseFloat($("#score").text())
+	"score": parseFloat(points)
   };
   window.parent.postMessage(msg, "*");
+  $("#over").show();
+  
+  $("#over > p").text( "Game Over, Your Points: " + points );
+  
+  streak = 0;
+  points = 0;
+  lives = 10;
 };
 
 // Sends this game's state to the service.
@@ -24,8 +33,9 @@ function saveStatus() {
   var msg = {
 	"messageType": "SAVE",
 	"gameState": {
-	  "streak": parseFloat($("#streak").text()),
-	  "score": parseFloat($("#score").text())
+	  "streak": parseFloat(streak),
+	  "lives": parseFloat(lives),
+	  "score": parseFloat(points)
 	}
   };
   window.parent.postMessage(msg, "*");
@@ -52,6 +62,8 @@ window.addEventListener("message", function(evt) {
   if(evt.data.messageType === "LOAD") {
 	streak = evt.data.gameState.streak;
 	$("#streak").text(streak);
+	lives = evt.data.gameState.lives;
+	showLives();
 	points = evt.data.gameState.score;
 	$("#score").text(points);
   } else if (evt.data.messageType === "ERROR") {
@@ -70,20 +82,65 @@ $(".playerButton").click( function() {
 	var comp = Math.floor((Math.random() * 3));
 	
 	$(".playerButton").removeClass("btn-info");
-	$(".compButton").removeClass("btn-danger");
+	$(".compButton").removeClass("btn-info");
+	$("#result").removeClass("btn-danger");
+	$("#result").removeClass("btn-success");
+	$("#result").removeClass("btn-warning");
 	$("#" + this.id).addClass("btn-info");
-	$("#Comp" + comp).addClass("btn-danger");
-	
-	//$("#compRPS").text( rpsNames[ comp ] );
-	
-	var resultText = "TIE";
+	$("#Comp" + comp).addClass("btn-info");
+
+	var resultText = "";
 	if( ( comp + 1 ) % 3 == parseInt( this.id ) )
+	{
+		streak = 0;
+		lives--;
+		$("#result").addClass("btn-danger");
 		resultText = "YOU LOST";
+	}
 	else if( ( parseInt( this.id ) + 1 ) % 3 == comp )
+	{
+		$("#result").addClass("btn-success");
 		resultText = "YOU WON";
-		
-	$("#result").text( resultText );
+		streak++;
+		points += streak;
+	}
+	else
+	{
+		resultText = "TIE";
+		$("#result").addClass("btn-warning");
+	}
+	
+	if( lives <= 0 )
+		gameOver();
+	else
+		updateStatus(resultText);
 });
+
+$("#close").click( function() {
+	$("#over").hide();
+	updateStatus("New Game");
+});
+
+function updateStatus(resultText) {
+	$("#streak").text(streak);
+	$("#score").text(points);
+	$("#result").text(resultText);
+	showLives();
+}
+
+function showLives() {
+	var img = $("<img/>");
+	img.attr("src", "heart.png");
+	img.attr("width", "25");
+	img.attr("height", "25");
+	img.attr("alt", "Lives left");
+	var livesElem =  $("#lives");
+	livesElem.html("");
+	for( var i = 0; i < lives ; i++ )
+	{
+		livesElem.append( img.clone() );
+	}
+}
 
 // Request the service to set the resolution of the
 // iframe correspondingly
@@ -96,5 +153,18 @@ var message =  {
 };
 window.parent.postMessage(message, "*");
 loadRequest();
+showLives();
+
+$('#popover').popover({
+    trigger: 'click',
+    content: '<p class="guide">Click Player\'s Rock/Scissors/Paper. ' +
+			'Your get points when you win a round against computer. Points are increased by streak amount. ' +
+			'Streak increases when you win. Streak is cleared when you lose. ' + 
+			'You lose live when you lose a round. Game ends when all lives are gone.</p>' +
+			'<p class="guide">Game is saved automatically after each round and loaded when you launch the game. ' +
+			'High scores are also updated to the game service.</p>',
+    placement: 'top',
+    html: true
+  });
 
 });
